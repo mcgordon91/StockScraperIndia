66273541691e3695e43e9b7f35d791c12dba071f
Michael Chang Gordon <mcgordon3478@gmail.com>
2021-05-23 10:21:59 -0500

    First Commit

---
+++ SharpeRatio.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('fivethirtyeight')
